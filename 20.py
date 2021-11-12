b96be044c3ecc0e145a5f3a370bd40413c8f0339
Matija Zinic <matija.zinic15@gmail.com>
2021-11-12 21:32:57 +0100

    Dodatno rijeseni

--- 20.py
+++ 20.py
@@ -3,3 +3,14 @@
 Sortirajte sve unose koristeći druge vrijednosti iz svakog unosa.
 """
 
+n = int(input("Unesite n: "))
+arr = list()
+for i in range(n):
+    a = int(input("Unesite prvi broj: ")) #Ograničeno na cijele brojeve
+    b = int(input("Unesite drugi broj: "))
+    print()
+    arr.append([a, b])
+
+print(f"Lista prije sortiranja {arr}")
+rez = sorted(arr, key=lambda x: x[1])
+print(f"Lista nakon sortiranja {rez}")
---
+++ 22.py
@@ -0,0 +1,30 @@
+"""
+22. Napravite funkciju koja prima brojeve i vraća redom:
+	a) umnožak tih brojeva
+	b) zbroj tih brojeva 
+	c) poredak po veličini (descending)
+	d) tuple prostih brojeva koji su unešeni
+	e) tuple parnih brojeva koji su unešeni
+	*uzmite rezultate te funkcije te ih zapišite u file def_rez.txt
+
+"""
+
+#Napravit ću samo pronalazak prostih brojeva i spremanje u tekstualnu datoteku
+
+def je_li_prost(n):
+
+	prost = True
+	for i in range(2, round(n**(1/2) + 1)):
+		if not n % i:
+			prost = False
+	return True if prost and n > 1 else False
+
+arr = [1, 2, 7, 10, 16, 18, 19]
+result = filter(je_li_prost, arr)
+
+string_ints = [str(elem) for elem in result]
+str_of_ints = ",".join(string_ints)
+
+f = open("def_rez.txt","w+")
+f.write(str_of_ints)
+f.close()
